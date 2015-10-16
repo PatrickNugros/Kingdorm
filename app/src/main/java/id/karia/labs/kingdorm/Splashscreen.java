8d5c50c6c0074f6c3d54b93787e855e390a4dccd
Patrick Nugros <patricknugros@gmail.com>
2015-10-16 11:15:40 +0700

    Initial commit

---
+++ app/src/main/java/id/karia/labs/kingdorm/Splashscreen.java
@@ -0,0 +1,49 @@
+package id.karia.labs.kingdorm;
+
+/**
+ * Created by patricknugros on 10/16/15.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Splashscreen extends Activity {
+
+    //Set waktu lama splashscreen
+    private static int splashInterval = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(i);
+
+
+                //jeda selesai Splashscreen
+                this.finish();
+            }
+
+            private void finish() {
+                // TODO Auto-generated method stub
+
+            }
+        }, splashInterval);
+
+    };
+
+}
