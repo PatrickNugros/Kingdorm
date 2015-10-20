9eac3b4f5fb9c986fda6db9620129afeab260680
Patrick Nugros <patricknugros@gmail.com>
2015-10-20 21:43:04 +0700

    add horizontalview

--- app/src/main/java/id/karia/labs/kingdorm/SocialFragment.java
+++ app/src/main/java/id/karia/labs/kingdorm/SocialFragment.java
@@ -10,7 +10,6 @@ import android.view.ViewGroup;
 
 public class SocialFragment extends Fragment {
 
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
